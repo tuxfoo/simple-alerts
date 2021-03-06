d86306eb017c77cef01d29fe5a6e761d051284c1
tuxfoo <tuxfoo@librem.one>
2021-03-06 18:23:32 +1300

    First Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# simple-alerts for nodecg
+
+This is WIP
+
+The goal is to be able to set up a multiple alerts for any purpose on live streams.
+
+There will be multiple layouts and customisation options.
+
+The alerts will be triggered by other software/scripts using a rest API.
---
+++ extension/index.js
@@ -0,0 +1,9 @@
+'use strict';
+const someDep = require('animate.css');
+
+module.exports = function (nodecg) {
+	// An array of alerts and their settings
+	nodecg.Replicant('alerts', { defaultValue: [{name: 'Alert1', message: '(name) just tipped (amount) (currency)', duration: 5000, media: "none" }] });
+	// The currently active aleart, changed via the API
+	nodecg.Replicant('activeAlert', { defaultValue: 0 });
+};
