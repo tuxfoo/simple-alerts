d26d969bc2f9dfbec58511cb6ba51866e9e1daba
tuxfoo <tuxfoo@librem.one>
2021-03-10 12:56:18 +1300

    Basic API implemented, now stores current message/alert in seperate replicant

--- README.md
+++ README.md
@@ -8,17 +8,16 @@ The alerts will be triggered/activated by other software/scripts using a rest AP
 
 ## known issues/To do
 
-* Finish creating the API.
-* Fix the issues in the panel.
-    * Stop the alerts from duplicating when pressing submit.
-    * Show the saved media file in the drop down menu after reloading page.
+* Finish creating the API. (Partially implemented for testing)
 * Add a queue for the alerts.
-* Add a "test" to the panel, prevent the alert's from displaying when the user presses submit.
+* Add a "test" to the dashboard panel.
 * Add other media types (At the moment this only supports webm)
-* Add a few diffent layouts
+* Add a few different layouts
     * Media left, message center
     * media left, message center top
     * media left, message top left
     * media fullscreen, message center (This is currently the default)
 * Add sound and volumes.
 * Customisable fonts and colours.
+* Stop alert from playing when page loads.
+* change duration from ms to seconds.
--- extension/index.js
+++ extension/index.js
@@ -1,30 +1,48 @@
 'use strict';
-var express = require('express');
-var app = express();
 
 module.exports = function (nodecg) {
 	// An array of alerts and their settings
-	nodecg.Replicant('alerts', { defaultValue: [{name: 'Alert1', message: '(name) just tipped (amount) (currency)', duration: 5000, media: "none" }] });
+	nodecg.Replicant('alerts', { defaultValue: [{name: 'Alert1', message: '', duration: 5000, media: 'none' }] });
 	// The currently active aleart, changed via the API
-	nodecg.Replicant('activeAlert', { defaultValue: 0 });
-};
+	nodecg.Replicant('activeAlert', { defaultValue: 0, persistent: false });
+	// Will use an array to queue up alerts
+	nodecg.Replicant('alertQueue', { defaultValue: [], persistent: false });
+	// Will use this to prevent alerts overlapping
+	nodecg.Replicant('isAlertPlaying', { defaultValue: false, persistent: false });
+	// Current alert message is stored here
+	nodecg.Replicant('activateAlert', { defaultValue: { activate: false, message: "" }, persistent: false});
 
+	const router = nodecg.Router();
+	const alerts = nodecg.Replicant('alerts');
+	const alertQueue = nodecg.Replicant('alertQueue');
+	const activeAlert = nodecg.Replicant('activeAlert');
+	const isAlertPlaying = nodecg.Replicant('isAlertPlaying');
 
-module.exports = function (nodecg) {
-    const router = nodecg.Router();
-		const alerts = nodecg.Replicant('alerts');
-    router.get('/test', (req, res) => {
-        res.send('OK!');
-    });
+	router.post('/alert', (req, res) => {
+			const alertName = req.body.name;
+			const message = req.body.message;
+			alerts.value.forEach(findAlert);
+			res.send("Alert will be added to queue");
+			function findAlert(value, index, array) {
+				if ( value.name == alertName ) {
+					value.message = message;
+					console.log(value.message);
+					activateAlert(index);
+				}
+			}
+	});
 
-		router.post('/alert', (req, res) => {
-				//alerts[0].value.name = "Tada";
-				const data = req.body.name;
-				const message = req.body.message;
-				alerts.value[0].message = message;
-				alerts.value[0].name = data;
-				res.send("Done " + data);
-		});
+	nodecg.mount('/simple-alerts', router); // The route '/my-bundle/customroute` is now available
 
-    nodecg.mount('/simple-alerts', router); // The route '/my-bundle/customroute` is now available
+	function activateAlert(ind) {
+			const activate = nodecg.Replicant('activateAlert');
+			// Bool alway's changes, in case message's are the same.
+			if (activate.value.activate == true ) {
+				activate.value.activate = false;
+			} else {
+				activate.value.activate = true;
+			}
+			activeAlert.value = ind;
+			activate.value.message = alerts.value[ind].message;
+	}
 };
